3a331eaf207e390d30a852e2c882232fcda1f729
priyansh agrawal <priyanshagarwal02may@gmail.com>
2021-07-18 11:19:19 +0530

    removed temp files

--- temp/global.py
+++ temp/global.py
@@ -1,3 +1,39 @@
-import os
 import sys
-import math
+import os
+import hashlib
+import socket
+import time
+import re
+import glob
+import subprocess
+import select
+import cmd
+import rlcompleter
+import atexit
+import random
+import shlex
+import tqdm
+import stat
+from datetime import datetime
+from os import name as nm
+import humanize
+
+
+if nm == 'nt':
+	from pyreadline import Readline
+	readline = Readline()
+else:
+	import readline
+
+readline.parse_and_bind('tab: complete') # tab completion
+histfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), '.pythonhistory') # history file
+
+
+try:
+	readline.read_history_file(histfile)
+except IOError:
+	print("ok!!")
+	pass
+
+atexit.register(readline.write_history_file, histfile)
+del histfile, readline, rlcompleter
